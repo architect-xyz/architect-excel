3d881927369ba9e1f9f1257b31c6c394cb99226b
Andrew Cho <a.cho@architect.co>
2025-04-02 05:24:32 -0400

    comment

--- src/taskpane.ts
+++ src/taskpane.ts
@@ -58,7 +58,7 @@ Office.onReady(() => {
       await removeStorageItem('ArchitectApiKey');
       await removeStorageItem('ArchitectApiSecret');
       safeForm.reset();
-      setStatus('Logged out!');
+      setStatus('Logged out! The Add-in will no longer have access to your credentials on the next restart.');
     } catch (err) {
       console.error("Logout failed:", err);
       setStatus(`Error: ${(err as Error).message}`);
